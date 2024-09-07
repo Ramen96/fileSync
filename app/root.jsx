b07069a899cab6f2c50f2311bc511b7b34c7fb8a
Ramen96 <eric.worsfold@gmail.com>
2024-09-06 21:10:38 -0400

    updated sidebar and created new file button

--- app/root.jsx
+++ app/root.jsx
@@ -1,23 +1,17 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import Index from "./routes/_index";
+
+import "./css/root.css";
 
 export default function App() {
   return (
     <html>
       <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Hello world!</h1>
         <Outlet />
 
         <Scripts />
@@ -25,4 +19,3 @@ export default function App() {
     </html>
   );
 }
-
---
+++ app/routes/_index.jsx
@@ -0,0 +1,16 @@
+import "../css/index.css";
+
+export default function Index() {
+  return (
+    <div className="main">
+      <div className="sidebar">
+        <section className="logo">
+          <h1>FileSync</h1>
+        </section>
+        <section className="nbWrapper">
+          <button className="newButton">New</button>
+        </section>
+      </div>
+    </div>
+  );
+}

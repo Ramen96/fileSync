63e939b2651482fc61a894719456404acaf3afbc
Ramen96 <eric.worsfold@gmail.com>
2024-09-07 16:03:05 -0400

    added search bar

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,10 +1,15 @@
 import "../css/index.css";
 import SideBar from "../components/SideBar/sidebar";
+const searchIcon  = "../assets/search.svg"
 
 export default function Index() {
   return (
     <div className="main">
       <SideBar />
+      <div className="searchbarwrapper flex-jc-ai w100">
+        <img className="searchIcon" src={searchIcon} alt="search icon" />
+        <input className="searchBar" type="text" placeholder="Search drive" />
+      </div>
     </div>
   );
 }

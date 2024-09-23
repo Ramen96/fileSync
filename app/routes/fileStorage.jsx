ff6e33b1adc15010f5fc21ae778472812e8a6102
Ramen96 <eric.worsfold@gmail.com>
2024-09-22 22:31:09 -0400

    created pathTraversalDetection function... working on if statement to controll writing to file system based on out put of pathTraversalDetection

--- app/routes/fileStorage.jsx
+++ app/routes/fileStorage.jsx
@@ -1,48 +1,43 @@
 import { json, unstable_parseMultipartFormData, unstable_createMemoryUploadHandler, unstable_composeUploadHandlers, unstable_createFileUploadHandler } from "@remix-run/node";
 
-function sanitizePath(path) {
-  const isTraversal = [
-    "../",
-    "%2e%2e%2f",
-    "%2e%2e/",
-    "..%2f",
-    "%2e%2e%5c",
-    "%2e%2e\'",
-    "..%5c",
-    "%252e%252e%255c",
-    "..%255c",
-    "..",
-    " / ",
-    "..%c0%af",
-    "..%c1%9c"
-  ]
 
-  isTraversal.forEach((i) => {
-    path.includes(i)
-      ? true
-      : false
-  })
 
-  // const fileList = filesUploaded[0];
-  // for (let i = 0; i < fileList.length; i++) {
-  //   const relitivePath = fileList[i].webkitRelativePath;
-  //   const traversalTrue = [...relitivePath.matchAll(isTraversal)]; 
-  //   console.log(traversalTrue)
-  //   if (traversalTrue != []) {
-  //     throw new Error("Path traversal blocked")
-  //   } 
-  //   console.log("relitive path: ", relitivePath);
-  // }
-}
 
 export const action = async ({ request }) => {
+  const pathTraversalDetection = (path) => {
+    const traversalPatterns = [
+        "../",
+        "%2e%2e%2f",
+        "%2e%2e/",
+        "..%2f",
+        "%2e%2e%5c",
+        "%2e%2e\'",
+        "..%5c",
+        "%252e%252e%255c",
+        "..%255c",
+        "..",
+        " / ",
+        "..%c0%af",
+        "..%c1%9c"
+    ];
+
+    let traversalDetection = undefined;
+
+    for (let i = 0; i < traversalPatterns.length; i++) {
+      let traversalState = path.includes(traversalPatterns[i]);
+      if (traversalState === true) {
+        traversalDetection = true;
+        break;
+      } else {
+        traversalDetection = false;
+      }
+    }
+    return traversalDetection;
+  }
+  
 
   // const uploadHandler = unstable_composeUploadHandlers(
-  //   unstable_createFileUploadHandler({
-  //     maxPartSize: 5_000_000,
-  //     file: ({ filename }) => filename,
-  //     directory: "cloud",
-  //   }),
+    
   //   unstable_createMemoryUploadHandler()
   // );
 
@@ -60,12 +55,22 @@ export const action = async ({ request }) => {
 
   for (let i = 0; i < webKitRelitivePathsArr.length; i++) {
     const relitivePath = webKitRelitivePathsArr[i][1]; 
+    const fileName = webKitRelitivePathsArr[i][0];
     // ToDo:
     // pass relitvePath to sanitizePath() 
     // return boolen value to determine if path traversal is dectected
     // if detected throw error and do not write files to system
     // else use relitve path as path to save inside of cloud/
     console.log("relitvePath: ", relitivePath);
+    if (pathTraversalDetection(relitivePath)) {
+      throw new Error("Warning: path traversal is true.");
+    } else {
+      unstable_createFileUploadHandler({
+        maxPartSize: 5_000_000,
+        file: ({ filename }) => filename,
+        directory: `cloud/${relitivePath}/${fileName}`,
+      });
+    }
   }
 
   return new Response(JSON.stringify(webKitRelitivePaths), {

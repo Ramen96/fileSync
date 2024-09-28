c8a0e9628bb9781851de7534c36ee157523d5f82
Ramen96 <eric.worsfold@gmail.com>
2024-09-28 15:29:20 -0400

    file saving works now

--- app/routes/fileStorage.jsx
+++ app/routes/fileStorage.jsx
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs/promises';
+import path from 'node:path';
 
 export const action = async ({ request }) => {
   const pathTraversalDetection = (path) => {
@@ -27,23 +28,23 @@ export const action = async ({ request }) => {
         break;
       } else {
         traversalDetection = false;
-      }
-    }
+      };
+    };
     return traversalDetection;
-  }
-
+  };
 
   const formData = await request.formData();
   const formDataObject = Object.fromEntries(formData);
   const webKitRelitivePath = [];
 
-  console.log(formDataObject);
   for (let file in formDataObject) {
     if (formDataObject[file] instanceof File) {
       const relitivePath = formDataObject[file].name;
       webKitRelitivePath.push(relitivePath);
-    }
-  }
+    } else {
+      throw new Error("Error: Not a file");
+    };
+  };
 
   const itteratePaths = () => {
     let pathSafe = false;
@@ -54,93 +55,29 @@ export const action = async ({ request }) => {
         throw new Error("Warning: path traversal is true.");
       } else {
         pathSafe = true;
-      }
-    }
+      };
+    };
     return pathSafe;
-  }
+  };
 
   if (itteratePaths()) {
-    // write to fs 
+    for (let file in formDataObject) {
+      if (formDataObject[file] instanceof File) {
+        const content = await formDataObject[file].arrayBuffer();
+        const fileName = formDataObject[file].name;
+        const filePath = path.join('./cloud', fileName);
+        try {
+          await fs.mkdir(path.dirname(filePath), { recursive: true });
+          await fs.writeFile(filePath, Buffer.from(content));
+        } catch (err) {
+          console.error(`Error writing file ${fileName}:`, err);
+          throw new Error(`Failed to write file ${fileName}`);
+        }
+      } else {
+        throw new Error("Error: Not a file");
+      }
+    }
   }
-
-  // const parseFormDataObj = await request.formData();
-  // const formData = unstable_composeUploadHandlers(
-  //   unstable_createMemoryUploadHandler()
-  // )
-  
-  // const test = await unstable_parseMultipartFormData(
-  //   request,
-  //   formData
-  // )
-
-  // console.log("relitivePaths: ", webKitRelitivePathsArr);
-  // console.log(parseFormDataObj)
-
-  // let triggerWriteData = false;
-  
-  
-  
-  // const webKitRelitivePaths = JSON.parse(formData.get("relitivePaths"));
-  // const webKitRelitivePathsArr = [...Object.entries(webKitRelitivePaths)];
-
-
-  // async function streamToBuffer(readableStream) {
-  //   return new Promise((resolve, reject) => {
-  //     const chunks = [];
-  //     readableStream.on("data", data => {
-  //       if (typeof data === 'string') {
-  //         chunks.push(Buffer.from(data, 'utf-8'));
-  //       } else if (data instanceof Buffer) {
-  //         chunks.push(data);
-  //       } else {
-  //         const jsonData = JSON.stringify(data);
-  //         chunks.push(Buffer.from(jsonData, 'utf-8'));
-  //       }
-  //     });
-  //     readableStream.on('end', () => {
-  //       resolve(Buffer.concat(chunks));
-  //     });
-  //     readableStream.on('error', reject);
-  //   });
-  // }
-
-  // const test = await request.formData();
-
-  // console.log(request)
-  
-
-  // const getRelitivePaths = () => {
-  //   streamToBuffer(test)
-  //     .then(fileData => {
-  //       // const formToObj = Object.fromEntries(new FormData(fileData));
-  //       // const webKitRelitivePathsArr = Object.entries(JSON.parse(formToObj.relitivePaths));
-  //       // console.log("aklsjdfklasj;dflkja;::::: ", webKitRelitivePathsArr);
-  //       console.log(fileData);
-  //     })
-  //     .catch(error => console.error('Error: ', error));
-  // }
-
-  // getRelitivePaths();
-
-  // console.log(triggerWriteData());
-
-  // if (triggerWriteData) {
-    // const newStream = formData.stream();
-
-    // const uploadHandler = unstable_composeUploadHandlers(
-    //   unstable_createFileUploadHandler({
-    //     maxPartSize: 5_000_000,
-    //     file: ({ filename }) => filename,
-    //     directory: "cloud",
-    //   }),
-    //   unstable_createMemoryUploadHandler()
-    // );
-  
-    // await unstable_parseMultipartFormData(
-    //   newStream,
-    //   uploadHandler
-    // );
-  // }
 
   const placehodler = {};
   return new Response(JSON.stringify(placehodler), {

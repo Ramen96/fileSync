1fae378b546288d1b793e847c350a777a8e5cb3a
Ramen96 <eric.worsfold@gmail.com>
2024-12-16 20:37:20 -0500

    removed unused code

--- app/utils/prisma.server.js
+++ app/utils/prisma.server.js
@@ -1,35 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// let prisma;
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-//   await prisma.$connect();
-// } else {
-//   if (!global.__db) {
-//     global.__db = new PrismaClient();
-//     await global.__db.$connect();
-//   }
-//   prisma = global.__db;
-// }
-
-// export { prisma };
-
-// import { PrismaClient } from "@prisma/client";
-
-// let prisma;
-
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.__db) {
-//     global.__db = new PrismaClient();
-//   }
-//   prisma = global.__db;
-// }
-
-// export { prisma };
-
-
 import { PrismaClient } from "@prisma/client";
 
 const createPrismaClient = () => {

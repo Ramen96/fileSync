89991738af4b596d56e2c1b4e714a8ea703a73cf
Ramen96 <eric.worsfold@gmail.com>
2024-12-18 21:55:38 -0500

    checking paths before proceeding

--- app/routes/fileStorage.jsx
+++ app/routes/fileStorage.jsx
@@ -7,7 +7,54 @@ export const action = async ({ request }) => {
   const formData = Object.fromEntries(parseRequest);
   const metadata = JSON.parse(formData.metadata);
 
-  console.log(metadata);
+  const pathTraversalDetection = (path) => {
+    const traversalPatterns = [
+      "../",
+      "%2e%2e%2f",
+      "%2e%2e/",
+      "..%2f",
+      "%2e%2e%5c",
+      "%2e%2e'",
+      "..%5c",
+      "%252e%252e%255c",
+      "..%255c",
+      "..",
+      " / ",
+      "..%c0%af",
+      "..%c1%9c",
+    ];
+
+    let traversalDetection = undefined;
+
+    for (let i = 0; i < traversalPatterns.length; i++) {
+      let traversalState = path.includes(traversalPatterns[i]);
+      if (traversalState === true) {
+        traversalDetection = true;
+        break;
+      } else {
+        traversalDetection = false;
+      }
+    }
+    return traversalDetection;
+  };
+
+  for (let i = 0; i < metadata.length; i++) {
+    const path = metadata[i].webkitRelativePath;
+    if (pathTraversalDetection(path) === true) {
+      console.error('Path traversal detection flagged: ', path);
+      return new Response(JSON.stringify({}), {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  }
+
+
+  // const formData = await request.formData();
+  // const formDataObject = Object.fromEntries(formData);
+  // const webKitRelitivePath = [];
 
   // for (let file in formDataObject) {
   //   if (formDataObject[file] instanceof File) {
@@ -17,6 +64,20 @@ export const action = async ({ request }) => {
   //     throw new Error("Error: Not a file");
   //   }
   // }
+
+  // const itteratePaths = () => {
+  //   let pathSafe = false;
+  //   for (let i = 0; i < webKitRelitivePath.length; i++) {
+  //     const path = webKitRelitivePath[i];
+  //     if (pathTraversalDetection(path)) {
+  //       pathSafe = false;
+  //       throw new Error("Warning: path traversal is true.");
+  //     } else {
+  //       pathSafe = true;
+  //     }
+  //   }
+  //   return pathSafe;
+  // };
 
   // if (itteratePaths()) {
   //   for (let file in formDataObject) {
@@ -40,24 +101,14 @@ export const action = async ({ request }) => {
   //         }
   //       };
 
-  //       // Changing here to write to new db tables
-
   //       const writeToDb = async () => {
-  //         await prisma.metadata.create({
-  //           id: id,
-  //           name: fileName,
-  //           file_type: fileType,
-  //           is_folder: is_folder,
-  //           created_at: new Date()
-  //         })
-
-  //         await prisma.hierarchy.create({
-  //           id: id,
-  //           // Figure out how to get parrent id to get parrent id...
-  //           // Step 1)  before passing to this action route get the id of the directory the user is currently in
-  //           //          and pass it as a param to this fucntion.
-  //           // step 2) if parrent id is the root parrent id is null
-  //           parent_id: parent_id
+  //         await prisma.file_data.create({
+  //           data: {
+  //             relitive_path: fileRelitvePath,
+  //             file_name: fileName,
+  //             file_type: fileType,
+  //             save_date: new Date()
+  //           }
   //         })
   //       }
 

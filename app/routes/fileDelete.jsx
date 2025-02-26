fb2d225c82a728f731bd6c86b0dbe43de77f837d
Ramen96 <eric.worsfold@gmail.com>
2025-02-25 22:14:09 -0500

    sending json data to action route

--- app/routes/fileDelete.jsx
+++ app/routes/fileDelete.jsx
@@ -1,6 +1,14 @@
+import { json } from "express";
 import { prisma } from "../utils/prisma.server"
 
 export const action = async ({ request }) => {
-  
-
+  try {
+    const body = await request.json();
+    console.log(body);
+    return new Response(JSON.stringify({
+      message: "200"
+    }, {status: 200}));
+  } catch (error) {
+    console.error(`Error processing request ${error}`);
+  }
 }

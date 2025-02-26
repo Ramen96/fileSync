fd21ee6cf56d60e115499bf1b508eb54cc693c52
Ramen96 <eric.worsfold@gmail.com>
2025-02-25 22:24:21 -0500

    gettint root id to be referenced later for recreating path to files to be deleted

--- app/routes/fileDelete.jsx
+++ app/routes/fileDelete.jsx
@@ -3,8 +3,22 @@ import { prisma } from "../utils/prisma.server"
 
 export const action = async ({ request }) => {
   try {
+    const initRoot = await prisma.hierarchy.findFirst({
+      where: {
+        parent_id: null,
+      }
+    });
+
+    const rootId = initRoot.id;
+
     const body = await request.json();
-    console.log(body);
+    
+    body.forEach(element => {
+      console.log(element);
+    });
+
+    console.log(`Root id: ${rootId}`);
+
     return new Response(JSON.stringify({
       message: "200"
     }, {status: 200}));

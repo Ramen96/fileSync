bcd7d9a0378669f81e79bee857bcfa111742597f
Ramen96 <eric.worsfold@gmail.com>
2025-03-11 22:32:26 -0400

    created function returning metadata for the parent folder

--- app/routes/fileDelete.jsx
+++ app/routes/fileDelete.jsx
@@ -34,29 +34,48 @@ export const action = async ({ request }) => {
       }
     }
 
+    // returns metadata for the parent folder
+    const getMetaData = async (id) => {
+      try {
+        const metadata = await prisma.hierarchy.findUnique({
+          where: {
+            id: id
+          },
+          include: {
+            metadata: true
+          }
+        })
+
+        return metadata.metadata;
+      } catch (error) {
+        console.error(`Error getting metadata: ${error}`);
+      }
+    }
+
+    const pathChunks = [];
     const getFilePath = async (metadata) => {
-      // need to test this not sure if this array will be reset to original value in recursive function calls
-      // I think it will but we shall see in testing
-      const pathChunks = [];
       // Use recursion to get path to root and reconstruct the path
       try {
         const objectId = metadata.id;
         const isFile = metadata.type === "file" ? true : false;
-        
         if (isFile) {
           const fileMetadata = await getParentId(objectId);
           pathChunks.push(fileMetadata.name);
           console.log(`pathChunks: ${pathChunks}`);
-          if (fileMetadata.parent_id === rootId) console.log('in root folder'); // this is here for testing
+          if (fileMetadata.parent_id !== rootId) {
+            console.log(await getMetaData(fileMetadata.parent_id));
+          } else {
+            console.log('in root folder');
+          }
         } else {
-          console.log(metadata)
+          console.log('not a file');
+          console.log(metadata);
         }
       } catch (error) {
         console.error(`Error constructing file path ${error}`);
         throw new Error(error);
       }
     }
-    
     body.forEach(element => {
       getFilePath(element);
     });

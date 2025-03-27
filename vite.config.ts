1de622a4232526f117c8eec472e0745bb4ae58f4
Ramen96 <eric.worsfold@gmail.com>
2025-03-26 22:07:20 -0400

    updated vite server proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      'ws': {
+        target: 'ws://filesync.home:3030',
+        ws: true,
+        rewriteWsOrigin: true
+      }
+    },
     hmr: {
       protocol: "ws",
       clientPort: 3030,

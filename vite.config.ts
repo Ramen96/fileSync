f3332fe499d15538f25160579706a1958ea08c56
Ramen96 <eric.worsfold@gmail.com>
2025-03-26 22:00:50 -0400

    updated vite hmr settings

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    hmr: {
+      protocol: "ws",
+      clientPort: 3030,
+      overlay: false
+    }
   },
   plugins: [remix({
     // ignoredRouteFiles: ["**/*.css"],

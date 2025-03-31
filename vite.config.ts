2fa3ebdaf5d237b2ad8722e6b5fc3643dca5fdbc
Ramen96 <eric.worsfold@gmail.com>
2025-03-30 21:58:42 -0400

    cleaned up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    proxy: {
-      'ws': {
-        target: 'ws://filesync.home:3030',
-        ws: true,
-        // rewriteWsOrigin: true
-        changeOrigin: true
-      }
-    },
+    allowedHosts: ["filesync.home"],
     hmr: {
-      host: 'ws://filesync.home:3030',
+      overlay: false,
       protocol: "ws",
-      clientPort: 3030,
-      overlay: false
-    },
-    allowedHosts: ["filesync.home"]
+      clientPort: 3030
+    }
   },
   plugins: [reactRouter()]
 });

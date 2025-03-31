b05600e88576cb0aba2e9651ce3423ff9bb14a50
Ramen96 <eric.worsfold@gmail.com>
2025-03-30 22:24:21 -0400

    fixed syntax error

--- app/components/Loading/loading.jsx
+++ app/components/Loading/loading.jsx
@@ -2,10 +2,10 @@ import "../../css/loading.css";
 
 export default function LoadingBars() {
   return(
-    <div class="spinner-square">
-      <div class="square-1 square"></div>
-      <div class="square-2 square"></div>
-      <div class="square-3 square"></div>
+    <div className="spinner-square">
+      <div className="square-1 square"></div>
+      <div className="square-2 square"></div>
+      <div className="square-3 square"></div>
     </div>
   )
 }

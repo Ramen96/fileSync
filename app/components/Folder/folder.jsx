a759f71d9a9e08fee10e9841b2f9f7e9110c9874
Ramen96 <eric.worsfold@gmail.com>
2025-04-20 16:53:17 -0400

    cleaned up code

--- app/components/Folder/folder.jsx
+++ app/components/Folder/folder.jsx
@@ -1,74 +1,91 @@
 import { useState } from "react";
-import folder from "../../../assets/yellow-folder.svg"
+import folder from "../../../assets/yellow-folder.svg";
 import "../../css/file-folder.css";
 
 export default function Folder({
   name,
-  id, 
+  id,
   handleFolderClick,
   updateDisplayNodes,
   isIcon,
-  handleDeleteQueue
+  handleDeleteQueue,
 }) {
-  
   const [checked, setChecked] = useState(false);
 
   const handleChecked = () => {
     const metadataObject = {
       id: id,
       name: name,
-      type: "folder"
-    }
+      type: "folder",
+    };
     if (checked === true) {
       setChecked(false);
       handleDeleteQueue(checked, metadataObject);
     } else {
       setChecked(true);
       handleDeleteQueue(checked, metadataObject);
-    };
-  }
+    }
+  };
 
-  return (
-    isIcon
-      ?
-        <div 
-          onClick={() => handleChecked()}
-          className="conA" onDoubleClick={() => {
-          handleFolderClick(id);
-        }}>
-          <div 
-            className="conB justifyStart">
-            <label className="cb-con test" onClick={(e) => e.stopPropagation()}>
-              <input checked={checked} onChange={e => console.log(e.target.value)} className="checkbox" type="checkbox" />
-              <span 
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleChecked();
-                }} className="checkmark"></span>
-            </label>
-          </div>
-          <div className="conB border-bottom">
-            <img className="folderImg margin1rem" src={folder} alt="folder" />
-          </div>
-          <div className="conB">
-            <h3 className="itemName">{name}</h3>
-          </div>
-        </div>
-      : 
-        <div onClick={() => handleChecked()} className="rowWrapper mainBorder vc" onDoubleClick={() => {
-          handleFolderClick(id);
-          }}>
-          <div className="centerAllFlex width100">
-            <img className="folderImgRow" src={folder} alt="folder" />
-            <p className="itemName marginNone textStart">{name}</p>
-          </div>
-          <label className="cb-con" onClick={(e) => e.stopPropagation()}>
-            <input checked={checked} onChange={e => console.log(e.target.value)} className="checkbox" type="checkbox" />
-            <span onClick={(e) => {
+  return isIcon ? (
+    <div
+      onClick={() => handleChecked()}
+      className="conA"
+      onDoubleClick={() => {
+        handleFolderClick(id);
+      }}
+    >
+      <div className="conB justifyStart">
+        <label className="cb-con test" onClick={(e) => e.stopPropagation()}>
+          <input
+            checked={checked}
+            onChange={(e) => console.log(e.target.value)}
+            className="checkbox"
+            type="checkbox"
+          />
+          <span
+            onClick={(e) => {
               e.preventDefault();
               handleChecked();
-            }} className="checkmark"></span>
-          </label>
-        </div>
-  )
+            }}
+            className="checkmark"
+          ></span>
+        </label>
+      </div>
+      <div className="conB border-bottom">
+        <img className="folderImg margin1rem" src={folder} alt="folder" />
+      </div>
+      <div className="conB">
+        <h3 className="itemName">{name}</h3>
+      </div>
+    </div>
+  ) : (
+    <div
+      onClick={() => handleChecked()}
+      className="rowWrapper mainBorder vc"
+      onDoubleClick={() => {
+        handleFolderClick(id);
+      }}
+    >
+      <div className="centerAllFlex width100">
+        <img className="folderImgRow" src={folder} alt="folder" />
+        <p className="itemName marginNone textStart">{name}</p>
+      </div>
+      <label className="cb-con" onClick={(e) => e.stopPropagation()}>
+        <input
+          checked={checked}
+          onChange={(e) => console.log(e.target.value)}
+          className="checkbox"
+          type="checkbox"
+        />
+        <span
+          onClick={(e) => {
+            e.preventDefault();
+            handleChecked();
+          }}
+          className="checkmark"
+        ></span>
+      </label>
+    </div>
+  );
 }

632e63b2b6dabfd5d82fc5d00e0e35d9b9edb53e
Ramen96 <eric.worsfold@gmail.com>
2025-04-22 18:01:30 -0400

    removed unused code

--- webSocketServer.js
+++ webSocketServer.js
@@ -1,32 +1,3 @@
-// import { WebSocketServer } from 'ws';
-
-// const wss = new WebSocketServer({ port: 3030 });
-
-// wss.on('connection', function connection(ws) {
-//   ws.send(JSON.stringify({ message: 'Client connected' }));
-
-//   ws.on('message', function message(data) {
-//     const msgObject = JSON.parse(data);
-
-//     switch (msgObject.action) {
-//       case 'reload':
-//         ws.send(JSON.stringify({ message: 'reload', id: msgObject.id }));
-//         break;
-
-//       case 'connection':
-//         console.log('Client connected');
-//         ws.send(JSON.stringify({ message: 'Hello client '}));
-//         break;
-//       default:
-//         ws.send(JSON.stringify({ message: false }));
-//     }
-
-//     console.log('received: %s', data);
-//   });
-
-//   ws.on('error', console.error);
-// });
-
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3030 });
@@ -55,5 +26,3 @@ wss.on('connection', function connection(ws) {
 
   ws.on('error', console.error);
 });
-
-export { wss };

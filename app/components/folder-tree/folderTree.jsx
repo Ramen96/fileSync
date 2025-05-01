0718361f6680783b2acc21214e607ce0ead49b6d
Ramen96 <eric.worsfold@gmail.com>
2025-04-30 22:18:55 -0400

    troubleshooting ws up date issues

--- app/components/folder-tree/folderTree.jsx
+++ app/components/folder-tree/folderTree.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { wsContext } from '../../utils/context';
-import LoadingBars from '../Loading/loading';
+import React, { useEffect, useState, useContext } from "react";
+import { wsContext } from "../../utils/context";
+import LoadingBars from "../Loading/loading";
 import folderIconColor from "../../../assets/open-yellow-folder.svg";
 import folderIconGray from "../../../assets/yellow-folder.svg";
 import file from "../../../assets/file2.svg";
@@ -17,14 +17,15 @@ export default function FolderTree({
   backHistory,
   setBackHistory,
   handleFolderClick,
-  pendingFileOperation,
+  // pendingFileOperation,
   displayNodeId,
   setPendingFileOperation,
 }) {
-
   const socket = useContext(wsContext);
   const [isExpanded, setIsExpanded] = useState(new Set());
   const [childNodesMap, setChildNodesMap] = useState(new Map());
+  const [localPendingFileOperation, setLocalPendingFileOperation] =
+    useState(false);
 
   async function handleExpandFolder(folderId, parent_id) {
     if (!isExpanded.has(folderId)) {
@@ -50,75 +51,90 @@ export default function FolderTree({
     }
   }
 
-  if (!childrenOfRootNode || pendingFileOperation) {
-    return <LoadingBars />;
-  }
+
+  // 1. get message from ws conncection
+  // 2. get data
+  // 3. data may need to be loaded from parent component if it is the root level of this function
+  // 4. memoize data 
 
   socket.addEventListener("message", async (event) => {
     const data = JSON.parse(event?.data);
     const message = data?.message;
 
-    if (message === 'reload') {
-      // fetch data 
+    if (message === "reload") {
+      // fetch data
       try {
+        setLocalPendingFileOperation(true);
         const reloadId = data?.id;
         const newChildNodes = await getChildNodes(reloadId);
         setChildNodesMap(newChildNodes);
+        setLocalPendingFileOperation(false);
       } catch (error) {
         console.log(`Error fetching data ${error}`);
       }
     }
   });
 
-  return childrenOfRootNode.map((child) => {
-    const isFolder = child.metadata?.is_folder === true;
-    return isFolder ? (
-      <React.Fragment key={child.metadata.id}>
-        <div
-          onClick={() => handleExpandFolder(child.metadata.id, child.parent_id)}
-        >
-          {isExpanded.has(child.metadata.id) ? (
-            <div className="sideItem">
-              <img
-                className="sideBarIcon"
-                src={folderIconColor}
-                alt="folderIcon"
+  if (!childrenOfRootNode) {
+    return <LoadingBars />;
+  }
+
+  if (localPendingFileOperation) {
+    return <LoadingBars />;
+  } else {
+    return childrenOfRootNode.map((child) => {
+      console.log('loging', childrenOfRootNode);
+      const isFolder = child.metadata?.is_folder === true;
+      return isFolder ? (
+        <React.Fragment key={child.metadata.id}>
+          <div
+            onClick={() =>
+              handleExpandFolder(child.metadata.id, child.parent_id)
+            }
+          >
+            {isExpanded.has(child.metadata.id) ? (
+              <div className="sideItem">
+                <img
+                  className="sideBarIcon"
+                  src={folderIconColor}
+                  alt="folderIcon"
+                />
+                <h3 className="sideItemName">{child.metadata.name}</h3>
+              </div>
+            ) : (
+              <div className="sideItem">
+                <img
+                  className="sideBarIcon"
+                  src={folderIconGray}
+                  alt="folderIcon"
+                />
+                <h3 className="sideItemName">{child.metadata.name}</h3>
+              </div>
+            )}
+          </div>
+          {isExpanded.has(child.metadata.id) && (
+            <div className="sideFolderDropDown" style={{ display: "block" }}>
+              <FolderTree
+                childrenOfRootNode={childNodesMap.get(child.metadata.id) || []}
+                showStateList={showStateList}
+                setShowStateList={setShowStateList}
+                getChildNodes={getChildNodes}
+                currentNodeId={currentNodeId}
+                setCurrentNodeId={setCurrentNodeId}
+                setForwardHistory={setForwardHistory}
+                backHistory={backHistory}
+                setBackHistory={setBackHistory}
+                handleFolderClick={handleFolderClick}
               />
-              <h3 className="sideItemName">{child.metadata.name}</h3>
-            </div>
-          ) : (
-            <div className="sideItem">
-              <img
-                className="sideBarIcon"
-                src={folderIconGray}
-                alt="folderIcon"
-              />
-              <h3 className="sideItemName">{child.metadata.name}</h3>
             </div>
           )}
+        </React.Fragment>
+      ) : (
+        <div className="sideItem" key={child.metadata.id}>
+          <img className="sideBarIcon" src={file} alt="fileIcon" />
+          <h3 className="sideItemName">{child.metadata.name}</h3>
         </div>
-        {isExpanded.has(child.metadata.id) && (
-          <div className="sideFolderDropDown" style={{ display: "block" }}>
-            <FolderTree
-              childrenOfRootNode={childNodesMap.get(child.metadata.id) || []}
-              showStateList={showStateList}
-              setShowStateList={setShowStateList}
-              getChildNodes={getChildNodes}
-              currentNodeId={currentNodeId}
-              setCurrentNodeId={setCurrentNodeId}
-              setForwardHistory={setForwardHistory}
-              backHistory={backHistory}
-              setBackHistory={setBackHistory}
-              handleFolderClick={handleFolderClick}
-            />
-          </div>
-        )}
-      </React.Fragment>
-    ) : (
-      <div className="sideItem" key={child.metadata.id}>
-        <img className="sideBarIcon" src={file} alt="fileIcon" />
-        <h3 className="sideItemName">{child.metadata.name}</h3>
-      </div>
-    );
-  });
-};
+      );
+    });
+  }
+}

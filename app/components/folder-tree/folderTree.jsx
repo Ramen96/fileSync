6b6f75453d18e44a3f0b025bc9955f93f02517b4
Ramen96 <eric.worsfold@gmail.com>
2025-08-01 00:37:29 +0100

    removed unused code

--- app/components/folder-tree/folderTree.jsx
+++ app/components/folder-tree/folderTree.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
-import { wsContext } from "../../utils/context";
 import LoadingBars from "../Loading/loading";
-import folderIconColor from "../../../assets/open-yellow-folder.svg";
-import folderIconGray from "../../../assets/yellow-folder.svg";
-import file from "../../../assets/file2.svg";
 import "../../css/folderTree.css";
 
 export default function FolderTree({
@@ -33,59 +29,6 @@ export default function FolderTree({
       }
     }
   };
-
-
-
-
-
-  // const renderFolderItem = (child) => {
-  //   const folderId = child.metadata.id;
-  //   const isOpen = !!expandedFolders[folderId];
-  //   const children = expandedFolders[folderId] || [];
-  //   const isLoading = loadingFolders[folderId];
-
-  //   return (
-  //     <React.Fragment key={folderId}>
-  //       <div
-  //         onClick={() => {
-  //           handleExpand(folderId); 
-  //           if (typeof handleFolderClick === "function") {
-  //             handleFolderClick(folderId); 
-  //           }
-  //         }}
-  //       >
-  //         <div className="sideItem">
-  //           <img
-  //             className="sideBarIcon"
-  //             src={isOpen ? folderIconColor : folderIconGray}
-  //             alt="folderIcon"
-  //           />
-  //           <h3 className="sideItemName">{child.metadata.name}</h3>
-  //         </div>
-  //       </div>
-  //       {isOpen && (
-  //         <div className="sideFolderDropDown" style={{ display: "block" }}>
-  //           {isLoading
-  //             ? <LoadingBars />
-  //             : <FolderTree
-  //                 childrenOfRootNode={children}
-  //                 getChildNodes={getChildNodes}
-  //                 handleFolderClick={handleFolderClick} 
-  //               />
-  //           }
-  //         </div>
-  //       )}
-  //     </React.Fragment>
-  //   );
-  // };
-
-  // const renderFileItem = (child) => (
-  //   <div className="sideItem" key={child.metadata.id}>
-  //     <img className="sideBarIcon" src={file} alt="fileIcon" />
-  //     <h3 className="sideItemName">{child.metadata.name}</h3>
-  //   </div>
-  // );
-
 
   const renderFolderItem = (child) => {
     const folderId = child.metadata.id;

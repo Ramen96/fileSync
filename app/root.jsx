77514b22a0d67ca4b1191534c620af52b4efa3c5
Ramen96 <eric.worsfold@gmail.com>
2025-08-19 02:28:59 +0100

    added viewport to the meta tag

--- app/root.jsx
+++ app/root.jsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
         <link rel="icon" href="../assets/folder.svg" type="image/svg+xml" id="favicon" />
         <title>FileSync</title>
         <Meta />
@@ -12,7 +13,6 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-
         <Scripts />
       </body>
     </html>
